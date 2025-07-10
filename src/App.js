a990c45c5ddd9d484264006bb6b8ae5dbb85a88d
Aktiar0403 <aktiardocuments@gmail.com>
2025-07-10 13:31:13 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import Card from "./components/Card";
+import Button from "./components/Button";
+
+export default function App() {
+  // ...full NephroGuide code (I’ll give below)
+}
